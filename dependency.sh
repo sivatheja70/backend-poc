dfbe1aaedd32779615499a050173159c7b934577
Chandana2510 <chandanagovada12@gmail.com>
2024-05-27 16:25:53 +0530

    adding files

--- dependency.sh
+++ dependency.sh
@@ -1,14 +1,18 @@
 #!/bin/bash
 
-# Find directories containing a package.json file but exclude the root directory
-directories=$(find . -mindepth 2 -name "package.json" -exec dirname {} \;)
+# List of directories where npm install should be run
+directories=("login" "property" "layers/nodejs" "dependencies/nodejs")
 
 # Iterate over each directory and run npm install
-for dir in $directories; do
+for dir in "${directories[@]}"; do
     echo "Running npm install in $dir"
-    (cd "$dir" && npm install)
-    if [ $? -ne 0 ]; then
-        echo "npm install failed in $dir"
-        exit 1
+    if [ -d "$dir" ]; then
+        (npm i && cd "$dir" && npm install)
+        if [ $? -ne 0 ]; then
+            echo "npm install failed in $dir"
+            exit 1
+        fi
+    else
+        echo "Directory $dir does not exist"
     fi
 done
