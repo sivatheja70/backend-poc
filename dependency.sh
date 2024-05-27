4255966f7a4043092adeed1c75e55885dfdb1745
Chandana2510 <chandanagovada12@gmail.com>
2024-05-27 16:15:18 +0530

    adding env file vars

--- dependency.sh
+++ dependency.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
 
-# List of directories where npm install should be run
-directories=("login" "property" "layers/nodejs" "dependencies/nodejs")
+# Find directories containing a package.json file but exclude the root directory
+directories=$(find . -mindepth 2 -name "package.json" -exec dirname {} \;)
 
 # Iterate over each directory and run npm install
-for dir in "${directories[@]}"; do
+for dir in $directories; do
     echo "Running npm install in $dir"
-    if [ -d "$dir" ]; then
-        (npm i && cd "$dir" && npm install)
-        if [ $? -ne 0 ]; then
-            echo "npm install failed in $dir"
-            exit 1
-        fi
-    else
-        echo "Directory $dir does not exist"
+    (cd "$dir" && npm install)
+    if [ $? -ne 0 ]; then
+        echo "npm install failed in $dir"
+        exit 1
     fi
 done
